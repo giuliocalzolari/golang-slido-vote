7801e33d156cd7ab27ca36da2c30dacb548b785c
Giulio Calzolari <giuliocalzolari@users.noreply.github.com>
2018-12-10 10:07:21 +0100

    add basic test

--- main.go
+++ main.go
@@ -1,24 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 	"time"
-	"bytes"
-	"flag"
+
 	"github.com/alexcesaro/log/stdlog"
 )
 
-
-
 type Response struct {
-	Name    string    `json:"name"`
+	Name string `json:"name"`
 }
-
 
 type AuthData struct {
 	AccessToken string `json:"access_token"`
@@ -26,7 +24,6 @@ type AuthData struct {
 	EventUserID int    `json:"event_user_id"`
 }
 
-	
 type VoteData struct {
 	EventQuestionID        int `json:"event_question_id"`
 	EventQuestionScore     int `json:"event_question_score"`
@@ -97,10 +94,9 @@ type EventData []struct {
 	} `json:"owner"`
 }
 
-
 func GetEventData(event_code string) EventData {
-    eventUrl := fmt.Sprintf("https://app2.sli.do/api/v0.5/events?code=%s", event_code)
-    // logger.Debug("eventUrl:", eventUrl)
+	eventUrl := fmt.Sprintf("https://app2.sli.do/api/v0.5/events?code=%s", event_code)
+	// logger.Debug("eventUrl:", eventUrl)
 	response, err := http.Get(eventUrl)
 	if err != nil {
 		fmt.Print(err.Error())
@@ -112,51 +108,49 @@ func GetEventData(event_code string) EventData {
 	}
 	var ev EventData
 	json.Unmarshal(responseData, &ev)
-    return ev
+	return ev
 }
 
-
 func AuthEvent(event_uuid string) AuthData {
-    authUrl := fmt.Sprintf("https://app2.sli.do/api/v0.5/events/%s/auth", event_uuid)
+	authURL := fmt.Sprintf("https://app2.sli.do/api/v0.5/events/%s/auth", event_uuid)
 	form := url.Values{}
 	body := bytes.NewBufferString(form.Encode())
-	rsp, err := http.Post(authUrl, "", body)
+	rsp, err := http.Post(authURL, "", body)
 	if err != nil {
 		panic(err)
 	}
 	defer rsp.Body.Close()
-	body_byte, err := ioutil.ReadAll(rsp.Body)
+	bodyByte, err := ioutil.ReadAll(rsp.Body)
 	if err != nil {
 		panic(err)
 	}
 	var auth AuthData
-    json.Unmarshal(body_byte, &auth)    
-    // logger.Debug("AccessToken:", auth.AccessToken)
-    return auth
+	json.Unmarshal(bodyByte, &auth)
+	// logger.Debug("AccessToken:", auth.AccessToken)
+	return auth
 }
 
-
 func VoteUp(auth AuthData, question_id string) VoteData {
-    var jsonStr = []byte(`{"score":"1"}`)
-    client := &http.Client{}
-    voteUrl := fmt.Sprintf("https://app2.sli.do/api/v0.5/events/%d/questions/%s/like", auth.EventID, question_id) 
-    // logger.Debug("voteUrl:", voteUrl)
-    req, err := http.NewRequest("POST", voteUrl, bytes.NewBuffer(jsonStr))
-    req.Header.Add("Authorization", fmt.Sprintf("Bearer %s",string(auth.AccessToken)))
-    req.Header.Set("Content-Type", "application/json")
-    rsp, err := client.Do(req)
+	var jsonStr = []byte(`{"score":"1"}`)
+	client := &http.Client{}
+	voteURL := fmt.Sprintf("https://app2.sli.do/api/v0.5/events/%d/questions/%s/like", auth.EventID, question_id)
+	// logger.Debug("voteUrl:", voteUrl)
+	req, err := http.NewRequest("POST", voteURL, bytes.NewBuffer(jsonStr))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", string(auth.AccessToken)))
+	req.Header.Set("Content-Type", "application/json")
+	rsp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
-    defer rsp.Body.Close()
-    // logger.Debug("VoteUp response Status:", rsp.Status)
-	body_byte, err := ioutil.ReadAll(rsp.Body)
+	defer rsp.Body.Close()
+	// logger.Debug("VoteUp response Status:", rsp.Status)
+	bodyByte, err := ioutil.ReadAll(rsp.Body)
 	if err != nil {
 		panic(err)
 	}
 	var vote VoteData
-    json.Unmarshal(body_byte, &vote)    
-    return vote
+	json.Unmarshal(bodyByte, &vote)
+	return vote
 }
 
 func printUsageErrorAndExit(format string, values ...interface{}) {
@@ -167,40 +161,38 @@ func printUsageErrorAndExit(format string, values ...interface{}) {
 	os.Exit(64)
 }
 
-
 var (
-    eventPtr = flag.String("event", "xxxxx", "event code")
-    voteCountPtr = flag.Int("votes", 42, "votes count")
-    questionPtr = flag.String("question", "zzzzz", "question id")
+	eventPtr     = flag.String("event", "xxxxx", "event code")
+	voteCountPtr = flag.Int("votes", 42, "votes count")
+	questionPtr  = flag.String("question", "zzzzz", "question id")
 )
 
-
 func main() {
-    
-    flag.Parse()
-    logger := stdlog.GetFromFlags()
-    if *eventPtr == "" {
+
+	flag.Parse()
+	logger := stdlog.GetFromFlags()
+	if *eventPtr == "" {
 		printUsageErrorAndExit("-event is required")
-    }
-    
-    if *questionPtr == "" {
-		printUsageErrorAndExit("-question is required")
-    }
-
-    if *voteCountPtr  < 1 {
-		printUsageErrorAndExit("-votes must me greater than 1")
-	}    
-
-    logger.Info("Starting sli.do voter")
-    logger.Debug("voting event: ", *eventPtr)
-    logger.Debug("voting count: ", *voteCountPtr)
-    logger.Debug("question to vote: ", *questionPtr)
-
-    event := GetEventData(*eventPtr)
-    var vote VoteData
-    for i := 0; i < *voteCountPtr; i++ {
-        auth := AuthEvent(event[0].UUID)
-        vote = VoteUp(auth, *questionPtr)
 	}
-    logger.Info("Final Vote: ", vote.EventQuestionScore)
+
+	if *questionPtr == "" {
+		printUsageErrorAndExit("-question is required")
+	}
+
+	if *voteCountPtr < 1 {
+		printUsageErrorAndExit("-votes must me greater than 1")
+	}
+
+	logger.Info("Starting sli.do voter")
+	logger.Debug("voting event: ", *eventPtr)
+	logger.Debug("voting count: ", *voteCountPtr)
+	logger.Debug("question to vote: ", *questionPtr)
+
+	event := GetEventData(*eventPtr)
+	var vote VoteData
+	for i := 0; i < *voteCountPtr; i++ {
+		auth := AuthEvent(event[0].UUID)
+		vote = VoteUp(auth, *questionPtr)
+	}
+	logger.Info("Final Vote: ", vote.EventQuestionScore)
 }
